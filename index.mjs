eebc5fd1a07eb493d903a88cc5f9a44dccf159f1
Manav Misra <manavm@visionwebsoft.com>
2020-03-05 11:37:15 -0600

    :goal_net: Wrap 'await' in 'try-catch'

--- index.mjs
+++ index.mjs
@@ -3,8 +3,12 @@ import { promises as fs } from "fs";
 
 // IIFE
 (async function() {
-  const data = JSON.parse(await fs.readFile("./db.json"));
+  try {
+    const data = JSON.parse(await fs.readFile("./db.json"));
 
-  // Add 'id' to each data item by adding 1 to the item's index.
-  data.forEach((d, i) => (d.id = i + 1));
+    // Add 'id' to each data item by adding 1 to the item's index.
+    data.forEach((d, i) => (d.id = i + 1));
+  } catch (error) {
+    console.error(error);
+  }
 })();
